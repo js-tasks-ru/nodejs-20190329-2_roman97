2cb64f6510aa12e247f9870b962092c0e246d856
Roman Konkin <k.ramzes.r@gmail.com>
2019-05-08 20:11:14 +0300

    fix config 6-module 2-task

--- 6-module/2-task/libs/connection.js
+++ 6-module/2-task/libs/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
-const config = require('config');
+const config = require('../config/default');
+const get = require('lodash/get');
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -8,4 +9,4 @@ mongoose.set('useCreateIndex', true);
 
 mongoose.plugin(beautifyUnique);
 
-module.exports = mongoose.createConnection(config.get('mongodb.uri'));
+module.exports = mongoose.createConnection(get(config, 'mongodb.uri'));
